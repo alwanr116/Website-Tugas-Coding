a7b02dd81873a20e1e802f51339a348f1da43fbd
alwanr116 <alrizki274@gmail.com>
2024-11-11 12:59:55 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<h1>Nama : Alwan Rizki</h1>
+<h1>Npm : G1C024008</h1>
+<h1>Program studi : Teknik Mesin</h1>
+<h1>Mata kuliah : Coding</h1>
+<h1>Kelas : B</h1>
+<h1>Kata Kata Mutitara : Kegagalan Adalah Awal Dari Kesuksesan</h1>
